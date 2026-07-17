d8fc9c9dfbeff6ace364cea4d2d8bb2206f8841a
Husain Mistry <30475404+mastermystery007@users.noreply.github.com>
2026-07-17 18:59:13 +0200

    Add UMP consent manager

---
+++ app/src/main/java/com/mysterybox/deduceit2/privacy/AdConsentManager.kt
@@ -0,0 +1,63 @@
+package com.mysterybox.deduceit2.privacy
+
+import android.app.Activity
+import android.content.Context
+import com.google.android.ump.ConsentForm.OnConsentFormDismissedListener
+import com.google.android.ump.ConsentInformation
+import com.google.android.ump.ConsentRequestParameters
+import com.google.android.ump.FormError
+import com.google.android.ump.UserMessagingPlatform
+
+class AdConsentManager private constructor(context: Context) {
+    private val consentInformation: ConsentInformation =
+        UserMessagingPlatform.getConsentInformation(context)
+
+    fun interface OnConsentGatheringCompleteListener {
+        fun consentGatheringComplete(error: FormError?)
+    }
+
+    val canRequestAds: Boolean
+        get() = consentInformation.canRequestAds()
+
+    val isPrivacyOptionsRequired: Boolean
+        get() =
+            consentInformation.privacyOptionsRequirementStatus ==
+                ConsentInformation.PrivacyOptionsRequirementStatus.REQUIRED
+
+    fun gatherConsent(
+        activity: Activity,
+        onConsentGatheringCompleteListener: OnConsentGatheringCompleteListener
+    ) {
+        val params = ConsentRequestParameters.Builder().build()
+
+        consentInformation.requestConsentInfoUpdate(
+            activity,
+            params,
+            {
+                UserMessagingPlatform.loadAndShowConsentFormIfRequired(activity) { formError ->
+                    onConsentGatheringCompleteListener.consentGatheringComplete(formError)
+                }
+            },
+            { requestConsentError ->
+                onConsentGatheringCompleteListener.consentGatheringComplete(requestConsentError)
+            }
+        )
+    }
+
+    fun showPrivacyOptionsForm(
+        activity: Activity,
+        onConsentFormDismissedListener: OnConsentFormDismissedListener
+    ) {
+        UserMessagingPlatform.showPrivacyOptionsForm(activity, onConsentFormDismissedListener)
+    }
+
+    companion object {
+        @Volatile
+        private var instance: AdConsentManager? = null
+
+        fun getInstance(context: Context): AdConsentManager =
+            instance ?: synchronized(this) {
+                instance ?: AdConsentManager(context.applicationContext).also { instance = it }
+            }
+    }
+}
